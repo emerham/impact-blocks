8476dd6181e121ed0c1e0dc734c4e5b1f54c4bef
Matthew Brabham <matthew.brabham@oregonstate.edu>
2015-12-10 12:41:11 -0800

    Created first block, Social Media

---
+++ templates/block--cos-impact-blocks--impact-social.tpl.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: brabhamm
+ * Date: 12/9/15
+ * Time: 4:40 PM
+ */
+?>
+<div class="imp-social-icons">
+  <a class="twitter" href="<?php print render($items['twitter'])?>"><i class="fa fa-twitter"></i></a>
+  <a class="facebook" href="<?php print render($items['facebook'])?>"><i class="fa fa-facebook"></i></a>
+  <a class="instagram" href="<?php print render($items['instagram'])?>"><i class="fa fa-instagram"></i></a>
+</div>
