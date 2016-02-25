bc7172fb943b36580e4ae6c39a3b291de124748d
Matthew Brabham <matthew.brabham@oregonstate.edu>
2016-02-25 15:34:01 -0800

    Updated template for tools and social, finolized module

--- templates/block--cos-impact-blocks--impact-social.tpl.php
+++ templates/block--cos-impact-blocks--impact-social.tpl.php
@@ -5,6 +5,9 @@
  * Date: 12/9/15
  * Time: 4:40 PM
  */
+drupal_add_css('//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css', array(
+  'type' => 'external'
+));
 ?>
 <div class="imp-social-icons">
   <a class="twitter" href="<?php print render($items['twitter'])?>"><i class="fa fa-twitter"></i></a>
--- templates/block--cos-impact-blocks--impact-tools.tpl.php
+++ templates/block--cos-impact-blocks--impact-tools.tpl.php
@@ -5,7 +5,26 @@
  * Date: 12/15/15
  * Time: 4:52 PM
  */
+drupal_add_css('//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css', array(
+  'type' => 'external'
+));
 ?>
+<div class="toolbox-container"
 <div class="impact-tools">
-  My Content will go here
+  <div class="row-fluid">
+    <?php foreach ($items as $var){ ?>
+    <div class="toolbox">
+      <a href="<?php print $var['url'];?>"><i class="fa <?php print $var['icon'];?>"></i>
+        <p><?php print $var['title'];?></p>
+      </a>
+      </div>
+    <?php
+    };
+    ?>
+  </div>
+  <div class="row-fluid">
+    <a href="<?php print variable_get('last_button_link')?>" class="beyond">
+      <i class="fa fa-globe" style="font-size:18px;"></i> <?php print variable_get('last_button_text')?></a>
+  </div>
+</div>
 </div>
