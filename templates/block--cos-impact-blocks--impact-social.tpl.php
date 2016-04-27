36ecfcc886b4968cf248ddcf4a5b4318ea40725d
Matthew Brabham <matthew.brabham@oregonstate.edu>
2016-04-27 09:53:42 -0700

    Updated coding standards

--- templates/block--cos-impact-blocks--impact-social.tpl.php
+++ templates/block--cos-impact-blocks--impact-social.tpl.php
@@ -3,14 +3,21 @@
  * Created by PhpStorm.
  * User: brabhamm
  * Date: 12/9/15
- * Time: 4:40 PM
+ * Time: 4:40 PM.
  */
 drupal_add_css('//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css', array(
-  'type' => 'external'
+  'type' => 'external',
 ));
+``
 ?>
 <div class="imp-social-icons">
-  <a class="twitter" href="<?php print render($items['twitter'])?>"><i class="fa fa-twitter"></i></a>
-  <a class="facebook" href="<?php print render($items['facebook'])?>"><i class="fa fa-facebook"></i></a>
-  <a class="instagram" href="<?php print render($items['instagram'])?>"><i class="fa fa-instagram"></i></a>
+  <a class="icon" id="twitter"
+     href="<?php print render($items['twitter']) ?>"><i
+      class="fa fa-twitter"></i></a>
+  <a class="icon" id="facebook"
+     href="<?php print render($items['facebook']) ?>"><i
+      class="fa fa-facebook"></i></a>
+  <a class="icon" id="instagram"
+     href="<?php print render($items['instagram']) ?>"><i
+      class="fa fa-instagram"></i></a>
 </div>
--- templates/block--cos-impact-blocks--impact-tools.tpl.php
+++ templates/block--cos-impact-blocks--impact-tools.tpl.php
@@ -1,30 +1,34 @@
 <?php
 /**
+ * @file
  * Created by PhpStorm.
  * User: brabhamm
  * Date: 12/15/15
- * Time: 4:52 PM
+ * Time: 4:52 PM.
  */
 drupal_add_css('//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css', array(
-  'type' => 'external'
+  'type' => 'external',
 ));
 ?>
 <div class="toolbox-container"
 <div class="impact-tools">
   <div class="row-fluid">
-    <?php foreach ($items as $var){ ?>
-    <div class="toolbox">
-      <a href="<?php print $var['url'];?>"><i class="fa <?php print $var['icon'];?>"></i>
-        <p><?php print $var['title'];?></p>
-      </a>
+    <?php foreach ($items as $var) { ?>
+      <div class="toolbox">
+        <a href="<?php print $var['url']; ?>"><i
+            class="fa <?php print $var['icon']; ?>"></i>
+          <p><?php print drupal_strtoupper($var['title']); ?></p>
+        </a>
       </div>
-    <?php
+      <?php
     };
     ?>
   </div>
   <div class="row-fluid">
-    <a href="<?php print variable_get('last_button_link')?>" class="beyond">
-      <i class="fa fa-globe" style="font-size:18px;"></i> <?php print variable_get('last_button_text')?></a>
+    <a href="<?php print variable_get('last_button_link') ?>" class="beyond">
+      <i class="fa fa-globe"
+         style="font-size:18px;"></i> <?php print variable_get('last_button_text') ?>
+    </a>
   </div>
 </div>
 </div>
